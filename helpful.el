b298893c99a0d89c89159f492063331aa0dd9990
Jimmy Yuen Ho Wong <wyuenho@gmail.com>
2025-11-02 11:29:37 +0000

    Use help-fns internal symbol completion table

--- helpful.el
+++ helpful.el
@@ -2674,7 +2674,10 @@ Returns the symbol."
            (rx ": " eos)
            (format " (default: %s): " default-val)
            prompt)))
-  (intern (completing-read prompt obarray
+  (intern (completing-read prompt
+                           (if (fboundp 'help--symbol-completion-table)
+                               'help--symbol-completion-table
+                             obarray)
                            predicate t nil nil
                            default-val)))
 
